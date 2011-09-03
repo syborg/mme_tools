9232c21b3ed443f8579f06fb0c2bb50758ff24cc
Marcel Massana <xaxaupua@gmail.com>
2011-09-03 19:10:12 +0200

    tests are left to other files recalled from here

--- test/test_mme_tools.rb
+++ test/test_mme_tools.rb
@@ -1,19 +1,4 @@
 require 'test/unit'
 require 'mme_tools'
 
-A=1..12
-B="A".."M"
-C=%w{tantmateix adhuc nogensmenys urgell pocasolta carrincló}
-
-class TC_MmeTools < Test::Unit::TestCase
-
-  def setup
-  end
-
-  def teardown
-  end
-  
-  def test_compose
-    assert_equal [2,"B","adhuc"], MMETools::Enumerable.compose(A,B,C)[1]
-  end
-end
+require *(Dir[File.join("**","tc_*")])  # any file beginning in 'tc_' here or in subdirs
