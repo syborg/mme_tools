a0e377fb0e08be81ce910d9a2e78c3a5552717d4
Marcel Massana <xaxaupua@gmail.com>
2011-09-03 19:11:03 +0200

    added TODO

--- lib/mme_tools.rb
+++ lib/mme_tools.rb
@@ -6,7 +6,7 @@ require 'mme_tools/webparse'
 require 'mme_tools/debug'
 require 'mme_tools/concurrent'
 
-# TODO translate all documentation to english
+#TODO translate all documentation to english
 
 # Comprises various classes and methods 
 module MMETools
