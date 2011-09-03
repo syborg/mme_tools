11709b9784524536d6b2e27630a9db0a8ef9dcf0
Marcel Massana <xaxaupua@gmail.com>
2011-09-04 01:08:39 +0200

    First release

--- lib/mme_tools/version.rb
+++ lib/mme_tools/version.rb
@@ -6,8 +6,8 @@ module MMETools
 
     MAJOR = 0
     MINOR = 0
-    PATCH = 0
-    BUILD = 'pre2'  # use nil if not used
+    PATCH = 1
+    BUILD = nil  # use nil if not used
 	
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join(".")
 
