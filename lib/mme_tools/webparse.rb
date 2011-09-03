c8062ad6ee258ba4d3cabc336923f18a6abf035b
Marcel Massana <xaxaupua@gmail.com>
2011-09-04 01:05:24 +0200

    Fixed clear_string. There's an issue with the Iconv based implementation that makes, in some circumstances, transliterating all accented characters to '?'. So the impementation has changed to a pure ruby one based on the idea in http://www.jroller.com/obie/tags/unicode.

--- lib/mme_tools/webparse.rb
+++ lib/mme_tools/webparse.rb
@@ -3,6 +3,7 @@
 
 require 'rubygems'
 require 'iconv'
+require 'unicode'
 require 'jcode' if RUBY_VERSION < '1.9'
 
 $KCODE = "u"
@@ -28,11 +29,32 @@ module MMETools
     #   +:encoding+ => "ASCII" | "UTF8" (default)
     #     "ASCII" converteix tots els caracters al mes semblant ASCII (amb Iconv)
     #     "UTF8" torna una cadena UTF8
-    # a falta d'algo millor utilitzo Iconv.conv, pero es carrega tots els UTF8 i
-    # els aproxima al ASCII mes proxim
+    # (based on an idea of Obie Fernandez http://www.jroller.com/obie/tags/unicode)
     def clear_string(str, options={:encoding=>'UTF8'})
-      str=Iconv.conv('ASCII//TRANSLIT//IGNORE', 'UTF8', str) if options[:encoding]=='ASCII'
+      str=str.chars.map { |c| (c[0] <= 127) ? c : translation_hash[c] }.join if options[:encoding]=='ASCII'
       str.gsub(/[\s\302\240]+/mu," ").strip # el caracter UTF8 "\302\240" correspon al &nbsp; de HTML
+    end
+## La versio original basada en Iconv no funciona en algunes circumstancies
+#    def orig_clear_string(str, options={:encoding=>'UTF8'})
+#      str=Iconv.conv('ASCII//TRANSLIT//IGNORE', 'UTF8', str) if options[:encoding]=='ASCII'
+#      str.gsub(/[\s\302\240]+/mu," ").strip # el caracter UTF8 "\302\240" correspon al &nbsp; de HTML
+#    end
+
+    protected
+
+    def translation_hash
+      @@translation_hash ||= setup_translation_hash
+    end
+
+    def setup_translation_hash
+      accented_chars   = "ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝàáâãäåçèéêëìíîïñòóôõöøùúûüý".chars.map{|c| c}
+      unaccented_chars = "AAAAAACEEEEIIIIDNOOOOOxOUUUUYaaaaaaceeeeiiiinoooooouuuuy".split('')
+
+      translation_hash = {}
+      accented_chars.each_with_index { |char, idx| translation_hash[char] = unaccented_chars[idx] }
+      translation_hash["Æ"] = 'AE'
+      translation_hash["æ"] = 'ae'
+      translation_hash
     end
 
   end
