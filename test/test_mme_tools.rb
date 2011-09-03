619147c99f5e7a06fb5284654c06303d4d2f549f
Marcel Massana <xaxaupua@gmail.com>
2011-09-03 07:41:18 +0200

    provant el modul ngit del netbeans

--- test/test_mme_tools.rb
+++ test/test_mme_tools.rb
@@ -1,7 +1,19 @@
-require 'helper'
+require 'test/unit'
+require 'mme_tools'
 
-class TestMmeTools < Test::Unit::TestCase
-  def test_something_for_real
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+A=1..12
+B="A".."M"
+C=%w{tantmateix adhuc nogensmenys urgell pocasolta carrincló}
+
+class TC_MmeTools < Test::Unit::TestCase
+
+  def setup
+  end
+
+  def teardown
+  end
+  
+  def test_compose
+    assert_equal [2,"B","adhuc"], MMETools::Enumerable.compose(A,B,C)[1]
   end
 end
